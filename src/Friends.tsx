f1fef4b4ea22e89d56d53e43e766568d7f4b6f70
Jonny-exe <>
2020-12-13 10:48:46 +0100

    Changed the request url from ip address to localhost

--- src/Friends.tsx
+++ src/Friends.tsx
@@ -3,12 +3,12 @@ import Friend from './Friend'
 import { GetFriends } from './requests.jsx'
 
 const Friends = (props: any) => {
-  props.setReceiver(props.valueReceiver)
+  // props.setReceiver(props.valueReceiver)
   var { friends } = GetFriends(props.valueUser, props.friendAdded)
   console.log("Friends: ", friends)
   return (
     <div>
-      <div> {!friends ? "Loading friends" : friends.map((name: any) => (<Friend name={name} setReceiver={props.setReceiver} />))} </div>
+      {!friends ? "Loading friends" : friends.map((name: any) => (<Friend name={name} setReceiver={props.setReceiver} />))}
     </div>
   )
 }
--- src/requests.jsx
+++ src/requests.jsx
@@ -1,23 +1,24 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-const url = 'http://192.168.0.16:5000/';
+const url = 'http://localhost:5000/';
 const headersContent = {
+  // "Origin" : "http://localhost:3000"
   // "Access-Control-Allow-Credentials": "true",
-  // "Access-Control-Allow-Origin": "http://localhost:3000",
+  // "Access-Control-Allow-Origin": "*",
   // "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
   // "Access-Control-Max-Age": "1000",
   // "Access-Control-Allow-Headers": "x-requested-with, Content-Type, origin, authorization, accept, client-security-token",
-  // "Content-Type": "application/json"
+  // "Content-Type": "application/jso
 }
-
 export const PostRequest = (userContent, reciverContent, textContent) => {
   console.log("This is the texxt content: " + textContent)
+  const thisURL = url + "addmessage"
   var bodyContent = {
     sender: userContent,
     receiver: reciverContent,
     content: textContent
   }
-  fetch(url + "addmessage", {
+  fetch(thisURL, {
     method: 'POST',
     headers: headersContent,
     credentials: 'same-origin',
@@ -29,13 +30,14 @@ export const PostRequest = (userContent, reciverContent, textContent) => {
 }
 
 export const UploadProfileImage = (user, image, areaToCrop) => {
+  const thisURL = url + "uploadprofileimage"
   const bodyContent = {
     name: user,
     image: image,
-      areatocrop : areaToCrop
+    areatocrop: areaToCrop
   }
   console.log("UploadProfileImage", bodyContent)
-  fetch(url + "uploadprofileimage", {
+  fetch(thisURL, {
     method: 'POST',
     headers: headersContent,
     credentials: 'same-origin',
@@ -47,37 +49,38 @@ export const UploadProfileImage = (user, image, areaToCrop) => {
 }
 
 export const GetProfileImage = (user) => {
-  const [state, setState] = useState({picture: null, loading: true})
+  const [state, setState] = useState({ picture: null, loading: true })
+  const thisURL = url + "getprofileimage"
   useEffect(() => {
     var bodyContent = {
       name: user
     }
     console.log("GetProfileImage: bodyContent: ", bodyContent)
-    setState(state => ({picture: state.picture, loading: true}))
-    fetch(url + "getprofileimage", {
+    setState(state => ({ picture: state.picture, loading: true }))
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
       body: JSON.stringify(bodyContent)
     }).then(data => data.text()).then(text => JSON.parse(text)).then(json => {
       console.log("GetProfileImage: got image")
-      setState({picture: json, loading: false})
+      setState({ picture: json, loading: false })
     })
   }, [user])
   return state
 }
 
 export const UserLogin = (finalUser, finalPassword) => {
-  const [state, setState] = useState({successfulLogin: null, loginLoading: true})
-  const url = 'http://192.168.0.16:5000/login';
+  const [state, setState] = useState({ successfulLogin: null, loginLoading: true })
+  const thisURL = url + "login"
   useEffect(() => {
     var bodyContent = {
       name: finalUser,
       pass: finalPassword
     }
     console.log("Login: bodyContent: ", bodyContent)
-    setState({successfulLogin: null, loginLoading: true})
-    fetch(url, {
+    setState({ successfulLogin: null, loginLoading: true })
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
@@ -86,7 +89,7 @@ export const UserLogin = (finalUser, finalPassword) => {
       console.log("UserLogin: json data", json)
       if (json != null) {
         console.log("UserLogin: json set")
-        setState({successfulLogin: json, loginLoading: false})
+        setState({ successfulLogin: json, loginLoading: false })
       }
     })
   }, [finalPassword])
@@ -94,66 +97,66 @@ export const UserLogin = (finalUser, finalPassword) => {
 }
 
 export const DoesUserExist = (newUser) => {
-  const url = 'http://192.168.0.16:5000/doesuserexist';
-  const [state, setState] = useState({doesUserExist: null, registerLoading: true})
+  const thisURL = url + "doesuserexist"
+  const [state, setState] = useState({ doesUserExist: null, registerLoading: true })
   useEffect(() => {
     var bodyContent = {
       name: newUser
     }
     console.log("DoesUserExist: bodyContent: ", bodyContent, newUser)
-    setState({doesUserExist: null, registerLoading: true})
-    fetch(url, {
+    setState({ doesUserExist: null, registerLoading: true })
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
       body: JSON.stringify(bodyContent)
     }).then(data => data.text()).then(text => JSON.parse(text)).then(json => {
       console.log("DoesUserExist: json data", json, newUser)
-      setState({doesUserExist: json, registerLoading: false})
+      setState({ doesUserExist: json, registerLoading: false })
     })
   }, [newUser])
   return state
 }
 
 export const GetFriendRequests = (user) => {
-  const url = 'http://localhost:5000/getfriendrequests';
-  const [state, setState] = useState({requests: null, loading: true})
+  const thisURL = url + "getfriendrequests"
+  const [state, setState] = useState({ requests: null, loading: true })
   useEffect(() => {
     var bodyContent = {
       name: user
     }
     console.log("GetFriendRequests: bodyContent: ", bodyContent)
-    setState(state => ({requests: state.requests, loading: true}))
-    fetch(url, {
+    setState(state => ({ requests: state.requests, loading: true }))
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
       body: JSON.stringify(bodyContent)
     }).then(data => data.text()).then(text => JSON.parse(text)).then(json => {
       console.log("GetFriendRequests: json data: ", json)
-      setState({requests: json, loading: false})
+      setState({ requests: json, loading: false })
     })
   }, [])
   return state
 }
 
 export const GetWithFilter = (filterSender, filterReceiver) => {
-  const url = 'http://localhost:5000/getwithfilter';
-  const [state, setState] = useState({messages: null, loading: true})
+  const [state, setState] = useState({ messages: null, loading: true })
+  const thisURL = url + "getwithfilter"
   useEffect(() => {
     var bodyContent = {
       sender: filterSender,
       receiver: filterReceiver
     }
-    setState(state => ({messages: state.messages, loading: true}))
-    fetch(url, {
+    setState(state => ({ messages: state.messages, loading: true }))
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
       body: JSON.stringify(bodyContent)
     }).then(data => data.text()).then(text => JSON.parse(text)).then(json => {
       console.log("GetWithFilter: json data: ", json)
-      setState({messages: json, loading: false})
+      setState({ messages: json, loading: false })
     })
   }, [filterReceiver, filterSender])
   return state
@@ -161,15 +164,14 @@ export const GetWithFilter = (filterSender, filterReceiver) => {
 
 
 export const AddFriend = (userContent, newFriendContent) => {
-  const url = 'http://192.168.0.16:5000/addfriend';
-  // var jsonData
+  const thisURL = url + "addfriend"
   var bodyContent = {
     name: userContent,
     newFriend: newFriendContent
   }
 
   console.log("AddFriend: ", bodyContent)
-  fetch(url, {
+  fetch(thisURL, {
     method: 'POST',
     headers: headersContent,
     credentials: 'same-origin',
@@ -181,15 +183,14 @@ export const AddFriend = (userContent, newFriendContent) => {
 }
 
 export const AddFriendRequest = (userContent, newFriendContent) => {
-  const url = 'http://192.168.0.16:5000/addfriendrequest';
-  // var jsonData
+  const thisURL = url + "addfriendrequests"
   var bodyContent = {
     name: userContent,
     newFriend: newFriendContent
   }
 
   console.log("AddFriendRequest: ", bodyContent)
-  fetch(url, {
+  fetch(thisURL, {
     method: 'POST',
     headers: headersContent,
     credentials: 'same-origin',
@@ -201,15 +202,14 @@ export const AddFriendRequest = (userContent, newFriendContent) => {
 }
 
 export const RemoveFriendRequest = (user, friendToRemove) => {
-  const url = 'http://192.168.0.16:5000/removefriendrequest';
-  // var jsonData
+  const thisURL = url + "removefriendrequest"
   var bodyContent = {
     name: user,
     FriendToRemove: friendToRemove
   }
 
   console.log("RemoveFriendRequest: ", bodyContent)
-  fetch(url, {
+  fetch(thisURL, {
     method: 'POST',
     headers: headersContent,
     credentials: 'same-origin',
@@ -221,8 +221,7 @@ export const RemoveFriendRequest = (user, friendToRemove) => {
 }
 
 export const AddUser = (username, password) => {
-  // TODO: Maybe this doesnt work without capital letters. Test it
-  const url = 'http://192.168.0.16:5000/adduser';
+  const thisURL = url + "adduser"
   var bodyContent = {
     name: username,
     pass: password,
@@ -243,22 +242,22 @@ export const AddUser = (username, password) => {
 }
 
 export const GetFriends = (user, friendAdded) => {
-  const url = 'http://192.168.0.16:5000/getfriends';
-  const [state, setState] = useState({friends: null})
+  const [state, setState] = useState({ friends: null })
+  const thisURL = url + "getfriends"
 
   useEffect(() => {
     var bodyContent = {
       name: user
     }
-    setState(state => ({data: state.data}))
-    fetch(url, {
+    setState(state => ({ friends: state.friends }))
+    fetch(thisURL, {
       method: 'POST',
       headers: headersContent,
       credentials: 'same-origin',
       body: JSON.stringify(bodyContent)
     }).then(data => data.text()).then(text => JSON.parse(text)).then(json => {
       console.log("GetFriends: ", json)
-      setState({friends: json})
+      setState({ friends: json })
     })
   }, [user, friendAdded])
 
