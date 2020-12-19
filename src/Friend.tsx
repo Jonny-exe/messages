db7784a20ea3ade4a8b5e31b3964f6ce53831fb6
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2020-12-19 23:02:02 +0100

    Added some more styling

--- src/Friend.tsx
+++ src/Friend.tsx
@@ -10,7 +10,7 @@ function Friend(props: any) {
   console.log("Friend: picturea", picture)
   return (
     <div className="friend" onClick={setReceiver}>
-      <h3> {props.name} <img className="friendPicture" src={picture} alt="Image"></img></h3>
+      <h5 className="friendName"> {props.name} <img className="friendPicture" src={picture} alt="Image"></img></h5>
     </div>
   )
 }
